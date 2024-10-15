8ba37aeb9278b42255d7c4080d0598b6044686de
Wxlddd <lorenzopirozzi0@gmail.com>
2024-10-15 14:36:50 +0200

    Revert "Revert "Uploaded jupyter notebook""
    This reverts commit f5b26d52b98d80fbd64b5dc38bd8942d18402629.

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-from pathlib import Path
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
-# I dati sono stati ottenuti da Binance, maggior exchange di Crypto (?)
+cryptolist = ["ETH,SOL,XRP"]
 
-data_folder = Path("rawdata")
-filepath = data_folder / "Binance_BTCEUR_d.csv"
-df = pd.read_csv("rawdata/Binance_BTCEUR_d.csv")
-print(df.head())
+crypto1 = "BTC"
+crypto2 = "SOL"
+
+raw1 = pd.read_csv(f"{crypto1}EUR.csv", parse_dates=True).convert_dtypes()
+raw2 = pd.read_csv(f"{crypto2}EUR.csv", parse_dates=True).convert_dtypes()
+
+if len(raw1) < len(raw2):
+    raw2 = raw2.drop(range(0,len(raw2)-len(raw1)))
+else:
+    if len(raw1) > len(raw2):
+        raw1 = raw1.drop(range(0, len(raw1) - len(raw2)))
+
+close1 = raw1.loc[:, "Close"]
+close2 = raw2.loc[:, "Close"]
+
+plt.plot(close1, close2, "o")
+
+x = close1.to_numpy().reshape((-1, 1))
+y = close2.to_numpy()
